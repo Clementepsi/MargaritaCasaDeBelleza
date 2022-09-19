416c6bd4017339245a8c80745ae519f314b6082d
Ezequiel Lagunes <79433092+EzequielLagunes@users.noreply.github.com>
2022-09-18 21:28:41 -0500

    actualización de ortografía, botones y más

--- Registro/js/registroMain.js
+++ Registro/js/registroMain.js
@@ -66,3 +66,21 @@ function crearCuenta(e) {
 }
 
 
+	function mostrarPassword(){
+	var cambio = document.getElementById("txtPassword");
+	if(cambio.type == "password"){
+	cambio.type = "text";
+	$('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+	}else{
+	cambio.type = "password";
+	$('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+	}
+	} 
+											
+	$(document).ready(function () {
+	//CheckBox mostrar contraseña
+	$('#ShowPassword').click(function () {
+	$('#Password').attr('type', $(this).is(':checked') ? 'text' : 'password');
+	});
+	});
+								
