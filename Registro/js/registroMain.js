721820a8e909fd9b3fb21f96e30d948436d37aee
Clemente Flores <fmlclemente@hotmail.com>
2022-10-01 16:17:22 -0700

    borrar codigo comentado de registro main js

--- Registro/js/registroMain.js
+++ Registro/js/registroMain.js
@@ -1,89 +1,3 @@
-/*(function($) {
-
-	"use strict";
-
-	$(".toggle-password").click(function() {
-
-	  $(this).toggleClass("fa-eye fa-eye-slash");
-	  var input = $($(this).attr("toggle"));
-	  if (input.attr("type") == "password") {
-	    input.attr("type", "text");
-	  } else {
-	    input.attr("type", "password");
-	  }
-	});
-
-
-})(jQuery);
-
-//Variables campos
-const principalName = document.querySelector('#principalName');
-const lastName = document.querySelector('#lastName');
-const email = document.querySelector('#email');
-const txtPassword = document.querySelector('#txtPassword');
-
-const formularioEnviar = document.querySelector('#formRegistro');
-//variable boton
-const btnEnviar = document.querySelector('#boton_registro');
-//Event listeners
-
-eventListeners();
-function eventListeners() {
-  // document.addEventListener('DOMContentLoaded', desactivarBoton);
-
-  principalName.addEventListener('blur', validarFormulario);
-  lastName.addEventListener('blur', validarFormulario);
-  email.addEventListener('blur', validarFormulario);
-  txtPassword.addEventListener('blur', validarFormulario);
-  formularioEnviar.addEventListener('submit', crearCuenta);
-}
-
-// function desactivarBoton() {
-//   registro.setAttribute('disabled');
-// }
-
-function validarFormulario(e) {
-  if (e.target.value.length > 0) {
-    principalName.classList.remove('bg-danger');
-    lastName.classList.remove('bg-danger');
-    email.classList.remove('border', 'border-danger');
-    txtPassword.classList.remove('border', 'border-danger');
-  } else {
-    principalName.classList.add('border', 'border-danger');
-    lastName.classList.add('border', 'border-danger');
-    email.classList.add('border', 'border-danger');
-    txtPassword.classList.add('border', 'border-danger');
-  }
-}
-
-function crearCuenta(e) {
-  e.preventDefault();
-	const imagen = document.querySelector('#form_top')
-  const mensaje = document.createElement('p');
-  mensaje.textContent = 'Todos los campos son obligatorios';
-  mensaje.classList.add('bg-success');
-  mensaje.appendChild(imagen);
-}
-
-
-	function mostrarPassword(){
-	var cambio = document.getElementById("txtPassword");
-	if(cambio.type == "password"){
-	cambio.type = "text";
-	$('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
-	}else{
-	cambio.type = "password";
-	$('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
-	}
-	} 
-											
-	$(document).ready(function () {
-	//CheckBox mostrar contraseña
-	$('#ShowPassword').click(function () {
-	$('#Password').attr('type', $(this).is(':checked') ? 'text' : 'password');
-	});
-	});*/
-
 const header = document.querySelector('#header');
 const email = document.querySelector('#email');
 const form = document.querySelector('#form');
@@ -133,19 +47,19 @@ function validarFormulario(e) {
   }
 }
 document.querySelector('#btnSend').addEventListener("click", function(e){
-  let nombre = document.querySelector('#nombre');
-  let apellido = document.querySelector('#apellido');
-  let email = document.querySelector('#email');
-  let password = document.querySelector('#password');
-  console.log(nombre.value);
-  console.log(apellido.value);
-  console.log(email.value);
-  console.log(password.value);
+  let Nombre = document.querySelector('#nombre');
+  let Apellido = document.querySelector('#apellido');
+  let Correo = document.querySelector('#email');
+  let Password = document.querySelector('#password');
+  console.log(Nombre.value);
+  console.log(Apellido.value);
+  console.log(Correo.value);
+  console.log(Password.value);
   const data = {
-    nombre: nombre.value,
-    apellido: apellido.value,
-    email: email.value,
-    password: password.value
+    nombre: Nombre.value,
+    apellido: Apellido.value,
+    email: Correo.value,
+    password: Password.value
   };
   
   const url = "http://localhost:8080/api/clientes/";
